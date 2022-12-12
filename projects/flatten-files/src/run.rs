cd6018d44c9dacc7971bdcacc0a4c79f465a8d36
Aster <galaster@foxmail.com>
2022-12-12 13:48:19 +0800

    Add overwrite mode

--- projects/flatten-files/src/run.rs
+++ projects/flatten-files/src/run.rs
@@ -1,25 +1,17 @@
+use fs::rename;
+use std::ffi::OsStr;
 use std::fs;
 use std::path::Path;
 
+use diagnostic_quick::{QError, QResult};
 use diagnostic_quick::error_3rd::WalkDir;
-use diagnostic_quick::QResult;
 
 use crate::FlattenFlies;
 
 impl FlattenFlies {
     pub fn run<P>(&self, input: P) -> QResult where P: AsRef<Path> {
         let path = input.as_ref();
-        match try_run(self, path, true) {
-            Ok(_) => {}
-            Err(e) => {
-                Err(e)?
-            }
-        }
-        Ok(())
-    }
-    pub fn dry_run<P>(&self, input: P) -> QResult where P: AsRef<Path> {
-        let path = input.as_ref();
-        match try_run(self, path, false) {
+        match try_run(self, path) {
             Ok(_) => {}
             Err(e) => {
                 Err(e)?
@@ -29,7 +21,7 @@ impl FlattenFlies {
     }
 }
 
-fn try_run(cfg: &FlattenFlies, path: &Path, execute: bool) -> QResult {
+fn try_run(cfg: &FlattenFlies, path: &Path) -> QResult {
     let target = cfg.output.as_path();
     for entry in WalkDir::new(path) {
         let entry = entry?;
@@ -37,10 +29,40 @@ fn try_run(cfg: &FlattenFlies, path: &Path, execute: bool) -> QResult {
             let source = entry.path();
             let target = target.join(source.file_name().unwrap());
             println!("{} -> {}", entry.path().display(), target.display());
-            if execute {
-                fs::rename(source, target)?;
-            }
+            safe_rename(source, &target, cfg.execute)?;
         }
+    }
+    Ok(())
+}
+
+pub fn safe_rename(source: &Path, target: &Path, execute: bool) -> QResult {
+    if !target.exists() {
+        print_rename(source, target, execute)?;
+    }
+    let file_name = match target.file_name().and_then(OsStr::to_str) {
+        None => {
+            Err(QError::runtime_error("target file name is not valid"))?
+        }
+        Some(s) => {
+            s
+        }
+    };
+    let mut new_id = 1;
+    while new_id < 65535 {
+        let new_name = format!("{} ({})", file_name, new_id);
+        let new_target = target.with_file_name(new_name);
+        if !new_target.exists() {
+            return print_rename(source, &new_target, execute);
+        }
+        new_id += 1;
+    }
+    Ok(())
+}
+
+fn print_rename(source: &Path, target: &Path, execute: bool) -> QResult {
+    println!("{} -> {}", source.display(), target.display());
+    if execute {
+        rename(source, target)?;
     }
     Ok(())
 }
