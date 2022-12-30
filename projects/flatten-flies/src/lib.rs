b80ecf514188d64b5ed422f8d2810d6af16be924
Aster <galaster@foxmail.com>
2022-12-30 08:14:05 +0800

    🔧 Check empty directory

--- projects/flatten-flies/src/lib.rs
+++ projects/flatten-flies/src/lib.rs
@@ -4,17 +4,27 @@ mod run;
 
 pub struct FlattenFlies {
     pub output: PathBuf,
-    pub delete_empty: bool,
     pub pattern: String,
+    pub delete_empty: bool,
 }
 
 impl FlattenFlies {
     #[inline]
     pub fn new<P: AsRef<Path>>(output: P) -> Self {
+        debug_assert!(output.as_ref().is_dir());
         Self {
             output: output.as_ref().to_path_buf(),
-            delete_empty: false,
             pattern: "*".to_string(),
+            delete_empty: false,
         }
+    }
+}
+
+pub fn is_empty_directory(path: &Path) -> bool {
+    match path.read_dir() {
+        Ok(mut o) => {
+            o.next().is_none()
+        }
+        Err(_) => { false }
     }
 }
